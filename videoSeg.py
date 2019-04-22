d07a107fd08a7536f09f25e426a6f15033cbb609
EEEGUI <1462243304@qq.com>
2019-04-22 21:00:58 +0800

    update code

--- videoSeg.py
+++ videoSeg.py
@@ -1,6 +1,8 @@
+import os
 import cv2
 import yaml
 import torch
+import time
 from scipy import misc
 import numpy as np
 from ptsemseg.models import get_model
@@ -17,7 +19,7 @@ class VideoSegmentation:
         fourcc = cv2.VideoWriter_fourcc(*'DIVX')
         self.h = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.w = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.out = cv2.VideoWriter('outputs/seg_%s.avi' % filename, fourcc, 15.0, (int(self.w), int(self.h)))
+        self.out = cv2.VideoWriter('outputs/seg_%s.avi' % filename, fourcc, int(30/cfg['testing']['downsample']), (int(self.w), int(self.h)))
         self.model = self._load_model(cfg)
 
     def _load_model(self, cfg):
@@ -48,13 +50,13 @@ class VideoSegmentation:
             img = np.expand_dims(img, 0)
             # img = torch.from_numpy(img).float()
             batchimgs.append(torch.from_numpy(img))
+        del imgs
         batchimgs = torch.cat(batchimgs).float()
         batchimgs = batchimgs.to(self.device)
         batchimgs = self.model(batchimgs)
 
         imgs = []
         for img in batchimgs:
-            c = img.data.max(0)
             img = img.data.max(0)[1].cpu().numpy()
             img = img.astype(np.float32)
             # float32 with F mode, resize back to orig_size
@@ -70,7 +72,8 @@ class VideoSegmentation:
             ret, frame = self.cap.read()
             if ret is True:
                 print(i)
-                imgs.append(frame[:, :, ::-1])
+                if i % self.cfg['testing']['downsample'] == 0:
+                    imgs.append(frame[:, :, ::-1])
                 if i % self.cfg['testing']['bs'] == 0:
                     imgs = torch.from_numpy(np.array(imgs))
                     imgs = self.predict(imgs)
@@ -87,21 +90,13 @@ class VideoSegmentation:
         cv2.destroyAllWindows()
 
 
-def test_video_generate():
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out1 = cv2.VideoWriter('videos/video2.avi', fourcc, 15.0, (1280, 720))
-    img = cv2.imread("videos/VDO_0008.avi")
-
-    out1.write(np.uint8(img))
-    out1.release()
-
-
 if __name__ == '__main__':
-
-
-    # test_video_generate()
-    #
+    t1 = time.time()
     with open('configs/icnet_mapillary.yml') as fp:
         cfg = yaml.load(fp)
-    vs = VideoSegmentation(cfg, "videos/video1.avi")
-    vs.process()
+    for video in os.listdir(cfg['testing']['video_fold']):
+        video_path = os.path.join(cfg['testing']['video_fold'], video)
+        vs = VideoSegmentation(cfg, video_path)
+        vs.process()
+        del vs
+    print(time.time() - t1)
